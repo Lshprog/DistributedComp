f6c57abc52902024fd2d3d28b8a2bf880df90438
Lshprog <onisalex1@gmail.com>
2023-09-04 15:45:55 +0800

    init

---
+++ src/main/java/org/example/ui/UI.java
@@ -0,0 +1,24 @@
+package org.example.ui;
+
+import javax.swing.*;
+
+public class UI {
+
+    private JFrame jFrame;
+
+    private JSlider jSlider;
+
+    private JSpinner jSpinner1;
+
+    private JSpinner jSpinner2;
+
+
+    public UI() {
+
+        this.jFrame = new JFrame();
+        this.jSlider = new JSlider();
+        this.jSpinner1 = new JSpinner();
+        this.jSpinner2 = new JSpinner();
+
+    }
+}
